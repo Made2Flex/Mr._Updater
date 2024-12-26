05c6657e3d4dab71bd99253365a5069404343c10
Made2Flex <skywalka@duck.com>
2024-12-26 04:29:17 -0500

    Update mr_updater.sh

--- mr_updater.sh
+++ mr_updater.sh
@@ -779,7 +779,7 @@ update_system() {
             echo -e "${ORANGE}  >> Updates found. Proceeding with system update...${NC}"
             run_command "sudo pacman -Syyuu --noconfirm --needed --color=auto"
         else
-            echo -e "${ORANGE}==>> Pacman packages are up-to-date. No updates required.${NC}"
+            echo -e "${ORANGE}==>> Pacman packages are up-to-date.${NC}"
         fi
 
             # Use the global AUR_PACKAGES variable to determine AUR updates
